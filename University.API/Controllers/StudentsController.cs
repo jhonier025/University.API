fff53a0b063eb086d33de5ec98cde51be8aebf45
= <=>
2021-09-13 21:59:01 -0500

    Agregar archivos de proyecto.

---
+++ University.API/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Http;
+using University.BL.DTOs;
+using University.BL.Models;
+
+namespace University.API.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        private readonly UniversityEntities constext = new UniversityEntities();
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var students = constext.Student.ToList();
+            var studentsDTO = students.Select(x => new StudentDTO
+            {
+                ID = x.ID,
+                FirstMidName = x.FirstMidName,
+                LastName = x.LastName,
+                EnrollmentDate = x.EnrollmentDate.Value
+                
+            }).ToList();
+
+        
+            return Ok(studentsDTO);
+        }
+    }
+}
---
+++ University.BL/DTOs/StudentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.BL.DTOs
+{
+    public class StudentDTO
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+        public string FullName
+            {
+                get { return string.Format("{0}, {1}", LastName, FirstMidName); }
+            }
+        }
+}
